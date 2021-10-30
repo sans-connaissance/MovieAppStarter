c686a76da92e6af9e91de1d28111474c3426fc9c
sans-connaissance <davemalicke@gmail.com>
2021-10-30 18:16:47 -0400

    ADD AddActorViewModel

---
+++ MovieApp/View Models/AddActorViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  AddActorViewModel.swift
+//  MovieApp
+//
+//  Created by David Malicke on 10/30/21.
+//
+
+import Foundation
+import CoreData
+
+class AddActorViewModel: ObservableObject {
+    
+    var name: String = ""
+    
+    func addActorToMovie(movieId: NSManagedObjectID) {
+        
+        let movie: Movie? = Movie.byId(id: movieId)
+        
+        if let movie = movie {
+            let actor = Actor(context: Actor.viewContext)
+            actor.name = name
+            actor.addToMovies(movie)
+            
+            try? actor.save()
+        }
+        
+    }
+    
+}
+
