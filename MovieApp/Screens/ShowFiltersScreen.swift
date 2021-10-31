49f9a045b52613f5934ca2cb52052d5340f43953
sans-connaissance <davemalicke@gmail.com>
2021-10-31 11:45:08 -0400

    ADD ShowFilterScreen

---
+++ MovieApp/Screens/ShowFiltersScreen.swift
@@ -0,0 +1,100 @@
+//
+//  ShowFiltersScreen.swift
+//  MovieApp
+//
+//  Created by Mohammad Azam on 3/17/21.
+//
+
+import SwiftUI
+
+struct ShowFiltersScreen: View {
+    
+    @State private var releaseDate: String = ""
+    @State private var startDate: String = ""
+    @State private var endDate: String = ""
+    @State private var minimumRating: String = ""
+    @State private var movieTitle: String = ""
+    @State private var actorName: String = ""
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        Form {
+            
+            Section(header: Text("Search by release date")) {
+                TextField("Enter release date", text: $releaseDate)
+                HStack {
+                    Spacer()
+                    Button("Search") {
+                        
+                       
+                        
+                    }.buttonStyle(PlainButtonStyle())
+                    Spacer()
+                }
+            }
+            
+            Section(header: Text("Search by date range")) {
+                TextField("Enter start date", text: $startDate)
+                TextField("Enter end date", text: $endDate)
+                HStack {
+                    Spacer()
+                    Button("Search") {
+                        
+                       
+                    }.buttonStyle(PlainButtonStyle())
+                    Spacer()
+                }
+            }
+            
+            Section(header: Text("Search by date range or rating")) {
+                TextField("Enter start date", text: $startDate)
+                TextField("Enter end date", text: $endDate)
+                TextField("Enter minimum rating", text: $minimumRating)
+                HStack {
+                    Spacer()
+                    Button("Search") {
+                        
+                       
+                    }.buttonStyle(PlainButtonStyle())
+                    Spacer()
+                }
+            }
+            
+            Section(header: Text("Search by movie title begins with")) {
+                TextField("Enter movie title", text: $movieTitle)
+                HStack {
+                    Spacer()
+                    Button("Search") {
+                        
+                       
+                        
+                    }.buttonStyle(PlainButtonStyle())
+                    Spacer()
+                }
+            }
+            
+            Section(header: Text("Search by actor name")) {
+                TextField("Enter actor name", text: $actorName)
+                HStack {
+                    Spacer()
+                    Button("Search") {
+                        
+                       
+                        
+                    }.buttonStyle(PlainButtonStyle())
+                    Spacer()
+                }
+            }
+            
+        }
+        .navigationTitle("Filters")
+        .embedInNavigationView()
+    }
+}
+
+struct ShowFiltersScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowFiltersScreen()
+    }
+}
