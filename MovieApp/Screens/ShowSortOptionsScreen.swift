5fd2ec8744c6cc7198c05cdbd6e9aca54df9b309
sans-connaissance <davemalicke@gmail.com>
2021-11-01 10:47:53 -0400

    ADD ShowSortOptionsScreen

---
+++ MovieApp/Screens/ShowSortOptionsScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ShowSortOptionsScreen.swift
+//  MovieApp
+//
+//  Created by Mohammad Azam on 3/21/21.
+//
+
+import SwiftUI
+
+struct ShowSortOptionsScreen: View {
+    var body: some View {
+        Text("ShowSortOptionsScreen")
+    }
+}
+
+struct ShowSortOptionsScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowSortOptionsScreen()
+    }
+}
